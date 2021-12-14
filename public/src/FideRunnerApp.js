b56cd7271578f97e8136c92ad0a98738519f621e
StudentMauricioGranados <jgranados00042@ufide.ac.cr>
2021-12-13 20:19:48 -0600

    Adding sass library

--- public/src/FideRunnerApp.js
+++ public/src/FideRunnerApp.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { AppRouter } from './router/AppRouter'
 
+import './FideRunnerApp.scss';
 export const FideRunnerApp = () => {
     return (
         <Provider store={ store }>
